48c9bcf18d439649597ff84b33cfdd7cb3404365
suren67 <surendranpandi02@gmail.com>
2024-08-14 15:18:56 +0530

    test15.18

--- public/script.js
+++ public/script.js
@@ -1,26 +1,26 @@
 window.addEventListener("beforeunload", () => {
-  const endTime = new Date();
-  const duration = Math.round((endTime - startTime) / 1000); // Duration in seconds
+    const endTime = new Date();
+    const duration = Math.round((endTime - startTime) / 1000); // Duration in seconds
 
-  const visitData = {
-      startTime: startTime.toISOString(),
-      endTime: endTime.toISOString(),
-      duration,
-      clickCount,
-      contactClicks,
-      whatsappClicks,
-      viewMoreClicks,
-      textSelections
-  };
+    const visitData = {
+        startTime: startTime.toISOString(),
+        endTime: endTime.toISOString(),
+        duration,
+        clickCount,
+        contactClicks,
+        whatsappClicks,
+        viewMoreClicks,
+        textSelections
+    };
 
-  fetch("https://usertracking-test.up.railway.app/api/save-visit", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json"
-      },
-      body: JSON.stringify(visitData)
-  })
-  .then(response => response.json())
-  .then(data => console.log('Success:', data))
-  .catch(error => console.error("Error saving visit data:", error));
+    fetch("https://usertracking-test.up.railway.app/api/save-visit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(visitData)
+    })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error("Error saving visit data:", error));
 });
