133736704c8782d87d837c3baf89fe395cad6d8c
suren67 <surendranpandi02@gmail.com>
2024-08-14 18:34:26 +0530

    test_18.32

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB and start the server
-mongoose.connect('mongodb://localhost:27017/yourdbname', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/userTrackingDB', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(3000, () => console.log('Server running on port 3000')))
     .catch(err => console.error(err));
