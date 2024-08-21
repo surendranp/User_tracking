f1dcc581395bb37b2ad3f1c83b9cde3fb3a078e3
suren67 <surendranpandi02@gmail.com>
2024-08-21 13:45:10 +0530

    test_13.45

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ async function sendVisitDataEmail() {
 }
 
 // Schedule a task to send visit data every 12 hours
-nodeCron.schedule('0 */12 * * *', () => {
+nodeCron.schedule('* * * *', () => {
     console.log('Executing cron job to send visit data email');
     sendVisitDataEmail();
 });
